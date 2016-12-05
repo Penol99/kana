a1c220039e9c178aa6b3acfcd96e4ab66f113092
Migge-Station\Migge <migen9930@gmail.com>
2016-12-05 20:28:29 +0100

    Added input reaction with the kana text for the hiragana layout

--- app/src/main/java/com/example/administratr/application/MainActivity.java
+++ app/src/main/java/com/example/administratr/application/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.administratr.application;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,16 +18,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
+    //Button presses
     public void kanaButtonClick(View view)  {
+
         setContentView(R.layout.kana_layout);
     }
 
     public void hiraganaButtonClick(View view)  {
+
         setContentView(R.layout.hiragana_layout);
+        hiraganaQuiz();
     }
 
     public void katakanaButtonClick(View view)  {
+
         setContentView(R.layout.katakana_layout);
     }
+
+    // Quiz methods
+    private void hiraganaQuiz() {
+        final EditText hiraganaTextInput = (EditText) findViewById(R.id.hiragana_text_input);
+        final TextView hiraganaText = (TextView) findViewById(R.id.hiragana_text);
+        // Check if the input of the EditText is the same as the hiragana from the TextView
+        findViewById(R.id.hiragana_next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String hiragana = hiraganaTextInput.getText().toString();
+                if (hiragana.equals("hiragana")) {
+                    hiraganaText.setText("meme");
+                }
+            }
+        });
+    }
+
 }
