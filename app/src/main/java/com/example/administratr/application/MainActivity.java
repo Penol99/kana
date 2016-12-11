cfc7b6607303732e778b0e524ccbfbe12eef45fc
Migge-Station\Migge <migen9930@gmail.com>
2016-12-11 01:28:56 +0100

    Added buttons on the main menu to be used for choosing how many words you want, also added more hiragana words

--- app/src/main/java/com/example/administratr/application/MainActivity.java
+++ app/src/main/java/com/example/administratr/application/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.administratr.application;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -14,57 +14,12 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    int count = 0;
-    int randLimit = 9;
-    int hirWords = 8;
-    int amountOfHiraganaWords = hirWords;
-    Random rand = new Random();
-    ArrayList<String> hiraganaWords = new ArrayList<String>();
-    ArrayList<String> hiraganaAnswers = new ArrayList<String>();
-    ArrayList<String> hiraganaTranslation = new ArrayList<String>();
-    public void HiraganaAnswers(){
-        hiraganaAnswers.add("konbanwa");
-        hiraganaAnswers.add("niku");
-        hiraganaAnswers.add("ohayo");
-        hiraganaAnswers.add("oyasumi");
-        hiraganaAnswers.add("okaasan");
-        hiraganaAnswers.add("zenbu");
-        hiraganaAnswers.add("yakusoku");
-        hiraganaAnswers.add("gochisousama");
-        hiraganaAnswers.add("tabete io");
-    }
-    public void HiraganaWords() {
-        hiraganaWords.add("こんばんは");
-        hiraganaWords.add("にく");
-        hiraganaWords.add("おはよ");
-        hiraganaWords.add("おやすみ");
-        hiraganaWords.add("おかあさん");
-        hiraganaWords.add("ぜんぶ");
-        hiraganaWords.add("やくそく");
-        hiraganaWords.add("ごちそうさま");
-        hiraganaWords.add("たべていお");
-
-    }
-
-    public void HiraganaTranslation() {
-        hiraganaTranslation.add("Good evening");
-        hiraganaTranslation.add("Meat");
-        hiraganaTranslation.add("Good morning");
-        hiraganaTranslation.add("Good night");
-        hiraganaTranslation.add("Mother");
-        hiraganaTranslation.add("Everything/All");
-        hiraganaTranslation.add("Promise");
-        hiraganaTranslation.add("Thanks for the meal");
-        hiraganaTranslation.add("You can eat it");
-
-    }
-
-    @Override
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+    }
 
     //Button presses
     public void kanaButtonClick(View view)  {
@@ -73,9 +28,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void hiraganaButtonClick(View view)  {
+        startActivity(new Intent(MainActivity.this, HiraganaActivity.class));
 
-        setContentView(R.layout.hiragana_layout);
-        hiraganaQuiz();
     }
 
     public void katakanaButtonClick(View view)  {
@@ -83,49 +37,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.katakana_layout);
     }
 
-    public void restartButton() {
-        // Needs to get changed so that it will on the other alphabets and not just hiragana
-        setContentView(R.layout.hiragana_layout);
-    }
-    public  void menuButton() {
-        setContentView(R.layout.activity_main);
-    }
-    // Quiz methods
-    private void hiraganaQuiz() {
-        HiraganaWords();
-        HiraganaAnswers();
-        HiraganaTranslation();
-        final EditText hiraganaTextInput = (EditText) findViewById(R.id.hiragana_text_input);
-        final TextView hiraganaText = (TextView) findViewById(R.id.hiragana_text);
-        final TextView hiraganaTranslationText = (TextView) findViewById(R.id.english_translation);
-        hiraganaText.setText(hiraganaWords.get(count));
-        hiraganaTranslationText.setText(hiraganaTranslation.get(count));
-        //hiraganaTranslationText.setText(hiraganaTranslation.get(count));
-        // Check if the input of the EditText is the same as the hiragana from the TextView
-        findViewById(R.id.hiragana_next).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                amountOfHiraganaWords -= 1;
-                if (amountOfHiraganaWords == 0){
-                    setContentView(R.layout.result_layout);
 
-                }
-                final String hiragana = hiraganaTextInput.getText().toString();
-                if (hiragana.equals(hiraganaAnswers.get(count))) {
-                    hiraganaTextInput.setText("");
-                    // Removes the word that already has been shown from the array when answered to correctly
-                    hiraganaWords.remove(count);
-                    hiraganaAnswers.remove(count);
-                    hiraganaTranslation.remove(count);
-                    randLimit -= 1;
-                    count = rand.nextInt(randLimit);
-                    // Change the current word of the translation and the hiragana
-                    hiraganaText.setText(hiraganaWords.get(count));
-                    hiraganaTranslationText.setText(hiraganaTranslation.get(count));
-
-                }
-            }
-        });
-    }
 
 }
