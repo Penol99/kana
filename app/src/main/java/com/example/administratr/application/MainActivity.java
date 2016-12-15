bf073221fa538e34666ff24ed9a7b0acb7260101
Migge-Station\Migge <migen9930@gmail.com>
2016-12-15 10:27:45 +0100

    meme

--- app/src/main/java/com/example/administratr/application/MainActivity.java
+++ app/src/main/java/com/example/administratr/application/MainActivity.java
@@ -4,13 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,21 +17,20 @@ public class MainActivity extends AppCompatActivity {
 
     //Button presses
     public void kanaButtonClick(View view)  {
-
-        setContentView(R.layout.kana_layout);
+        
     }
 
     public void hiraganaButtonClick(View view)  {
-        startActivity(new Intent(MainActivity.this, HiraganaActivity.class));
-        HiraganaActivity.hir = true;
-        HiraganaActivity.kat = false;
+        startActivity(new Intent(MainActivity.this, kanaActivity.class));
+        kanaActivity.hir = true;
+        kanaActivity.kat = false;
 
     }
 
     public void katakanaButtonClick(View view)  {
-        startActivity(new Intent(MainActivity.this, HiraganaActivity.class));
-        HiraganaActivity.hir = false;
-        HiraganaActivity.kat = true;
+        startActivity(new Intent(MainActivity.this, kanaActivity.class));
+        kanaActivity.hir = false;
+        kanaActivity.kat = true;
     }
 
 
--- app/src/main/java/com/example/administratr/application/kanaActivity.java
+++ app/src/main/java/com/example/administratr/application/kanaActivity.java
@@ -27,7 +27,7 @@ public class kanaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.hiragana_layout);
+        setContentView(R.layout.kana_layout);
         kanaQuiz();
     }
 
